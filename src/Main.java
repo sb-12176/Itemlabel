2ab461fd8545411a55d68520802518aefea312c5
Travis <tsmolinski24@students.desu.edu>
2025-04-08 12:41:49 -0400

    added text to main and removed testing

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+import static java.lang.Thread.sleep;
+
 public class Main {
+
+    static Scanner scan = new Scanner(System.in);
+
+    public static void say(String text){
+        System.out.println(text);
+    }
+
+
     public static void main(String[] args) {
-        Peepy firstPeepy = new Peepy();
+        say("Hello welcome to the itemalbel game.");
+        say(";ets make youe a peepy. first lets choose a name.");
+        String firstName = scan.next();
+        //sleep(1000);
+
+
+
+        /*Peepy firstPeepy = new Peepy();
         System.out.println(firstPeepy.toString());
 
         Food firstFood = new Food("peanut", 10);
         System.out.println(firstFood.toString());
 
         firstPeepy.feed(firstFood);
+         */
     }
 }
